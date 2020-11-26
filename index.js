4400dca246abe0832573a107fb216144dbeea49c
David Federspiel <david@federnet.com>
2020-11-26 00:24:10 -0500

    try: remove action for testing

--- index.js
+++ index.js
@@ -36,25 +36,25 @@ app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // Example of handling all message actions
-slackInteractions.action({ actionId: "top-answer" }, (payload) => {
-  // Logs the contents of the action to the console
-  console.log("payload", payload);
+// slackInteractions.action({ actionId: "top-answer" }, (payload) => {
+//   // Logs the contents of the action to the console
+//   console.log("payload", payload);
 
-  // Send an additional message only to the user who made interacted, as an ephemeral message
-  respond({
-    text: "Thanks for your submission.",
-    response_type: "ephemeral",
-  });
+//   // Send an additional message only to the user who made interacted, as an ephemeral message
+//   respond({
+//     text: "Thanks for your submission.",
+//     response_type: "ephemeral",
+//   });
 
-  // If you'd like to replace the original message, use `chat.update`.
-  // Not returning any value.
-});
+//   // If you'd like to replace the original message, use `chat.update`.
+//   // Not returning any value.
+// });
 
 slackInteractions.action({}, (payload, respond) => {
   console.log("HERE");
 });
 
-app.use("/interactions", slackInteractions.requestListener());
+app.use("/interact", slackInteractions.requestListener());
 
 slackEvents.on("url_verification", (event) => {
   return {
